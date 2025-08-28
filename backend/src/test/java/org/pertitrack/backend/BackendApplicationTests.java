647aead61fe82c40ba75eaac5fa58d0c8b18c7a4
ad-altun <adaltun@web.de>
2025-08-28 23:41:52 +0200

    update .gitignore and add backend module

---
+++ backend/src/test/java/org/pertitrack/backend/BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package org.pertitrack.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
