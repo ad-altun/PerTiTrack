30ef203ecdd609bfadda03174cc0a98ba955437c
ad-altun <adaltun@web.de>
2025-09-02 12:35:50 +0200

    clean frontend components back, implementing left after API is complete including unit/integration tests.

--- frontend/src/components/home/BookingPanel.tsx
+++
@@ -1,9 +0,0 @@
-
-
-export default function BookingPanel() {
- return (
-  <div>
-    
-  </div>
- );
-};
--- frontend/src/components/home/WelcomeSection.tsx
+++
@@ -1,9 +0,0 @@
-
-
-export default function WelcomeSection() {
- return (
-  <div>
-    
-  </div>
- );
-};
