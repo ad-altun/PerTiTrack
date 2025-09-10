1a308724be388b96acad69625d36122bbc6a3846
ad-altun <adaltun@web.de>
2025-09-10 17:33:57 +0200

    refactor: useLogout hook with better error handling - Use useCallback for logout function optimization - Ensure credentials are cleared on both success and error

--- frontend/src/hooks/useLogout.ts
+++ frontend/src/hooks/useLogout.ts
@@ -2,35 +2,27 @@ import { useNavigate } from "react-router-dom";
 import { useLogoutMutation } from "../store/api/authApi.ts";
 import { useAppDispatch } from "../store/hook.ts";
 import { clearCredentials } from "../store/slices/authSlice.ts";
+import { useCallback } from "react";
 
 export const useLogout = () => {
     const navigate = useNavigate();
     const dispatch = useAppDispatch();
-    const [ logoutMutation, { isLoading, error, isSuccess } ] = useLogoutMutation();
+    const [ logoutMutation, { isLoading, error } ] = useLogoutMutation();
 
-
-    const logout = async () => {
+    const logout = useCallback(async () => {
         try {
             await logoutMutation().unwrap();
-
-            dispatch(clearCredentials());
         }
         catch (error) {
             console.error('Logout failed: ', error);
-
+        } finally {
             dispatch(clearCredentials());
             navigate('/auth/signin');
         }
-
-        if ( isSuccess ) {
-            navigate('/auth/signin');
-        }
-
-        return {
-            logout,
-            // isLoading,
-            // error,
-            // isSuccess,
-        };
+    }, [ logoutMutation, dispatch, navigate ]);
+    return {
+        logout,
+        isLoading,
+        error,
     };
 };
